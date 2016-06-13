fccc8ff51542152915f6750ec5e165f6767d855d
weitangli <weitangli@edayun.cn>
2016-06-13 18:50:57 +0800

    init

--- database.sql
+++ database.sql
@@ -1,20 +1,9 @@
 create database if not exists workflow_test;
 use workflow_test;
 
--- drop table if exists account;
--- create table if not exists account(
---   id int primary key auto_increment,
---   name varchar(15) not null,
---   password varchar(25) not null,
---   user_id int not null
--- );
--- insert into account(id, name, password, user_id) values(1, 'admin', 'admin', 1);
-
 --
 -- 工作流相关
 --
-create database if not exists copper_dev;
-use copper_dev;
 drop table COP_WORKFLOW_INSTANCE_ERROR;
 drop table COP_WORKFLOW_INSTANCE;
 drop table COP_WAIT;
@@ -152,6 +141,7 @@ create table COP_LOCK (
 -- 定时器
 --
 DROP TABLE `schedulerconfig`;
+
 CREATE TABLE `schedulerconfig` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'AI',
   `schedulerName` varchar(64) NOT NULL COMMENT 'scheduler名字, 应该在SchedulerDef里面定义',
---
+++ src/copper/helloworld/HelloWorldTestApplication.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2002-2014 SCOOP Software GmbH
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package copper.helloworld;
+
+import java.io.File;
+
+import org.copperengine.core.CopperException;
+import org.copperengine.core.tranzient.TransientScottyEngine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class HelloWorldTestApplication
+{
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorldTestApplication.class);
+    private static int NUMBER_OF_WORKFLOWS = 5000;
+
+    private TransientScottyEngine engine;
+
+    private HelloWorldTestApplication()
+    {
+    }
+
+    public static void main(String args[]) throws Exception
+    {
+        new HelloWorldTestApplication().run();
+    }
+
+    private void run() throws InterruptedException
+    {
+
+        // initialize the procession engine the default configuration and source directory for the workflow files
+        TransientEngineFactory factory = new TransientEngineFactory()
+        {
+            @Override
+            protected File getWorkflowSourceDirectory()
+            {
+                return new File("src/copper/helloworld/workflow");
+            }
+        };
+
+        //Startup the engine
+        engine = factory.create();
+
+        // HelloWorldService provides an API to be called by the external adapter and needs to notify the engine.
+        HelloWorldService.init(engine);
+
+        //start 5000 workflows with request data
+        for (int i = 0; i < NUMBER_OF_WORKFLOWS; i++)
+        {
+            try
+            {
+                // Question: why not return the wfinstance id in the run(...) method?
+                // String wfId = engine.run("HelloWorldWorkFlow", getNewData());
+                // logger.info("Process HelloWorldWorkFlow running with id="+wfId);
+                engine.run("HelloWorldWorkFlow", getNewData());
+            }
+            catch (CopperException e)
+            {
+                logger.error("copper error: ", e);
+            }
+        }
+
+        // wait for all workflow instances to finish
+        for (; engine.getNumberOfWorkflowInstances() > 0;)
+        {
+            logger.info(engine.getNumberOfWorkflowInstances() + " wf remaining..");
+            Thread.sleep(100);
+        }
+        engine.shutdown();
+    }
+
+    public HelloWorldRequest getNewData()
+    {
+        return new HelloWorldRequest("Developer");
+    }
+}
